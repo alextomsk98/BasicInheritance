e6f0a8f743819c24024d0902eed95cade4822a3a
alextomsk98 <shavlo.2016@mail.ru>
2018-02-06 15:45:27 +0700

    laba3.1
    6.2.2018

---
+++ BasicInheritance/BasicInheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Interitance*****\n");
+            MiniVan myVan = new MiniVan();
+
+            myVan.Speed = 10;
+            Console.WriteLine("My van is going{0} MPH", myVan.Speed);
+            myVan.currSpeed = 55;
+            Console.ReadLine();
+        }
+    }
+}
